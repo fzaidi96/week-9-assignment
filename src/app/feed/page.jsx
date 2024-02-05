c1f004190d88f6aa813d4f7e99487ac8fd5124a7
fzaidi96 <f.zaidi182@gmail.com>
2024-02-05 14:11:19 +0000

    edit feed

--- src/app/feed/page.jsx
+++ src/app/feed/page.jsx
@@ -8,7 +8,11 @@ import NewPost from "@/components/NewPost";
 
 export default async function Feed({searchParams}) {
   const show = searchParams?.show;
-  const posts = await sql`SELECT * FROM posts`
+  const posts = await sql`
+  SELECT posts.*, profiles.username
+  FROM posts
+  JOIN profiles ON posts.profile_id = profiles.id
+`;
 
   return (
   <div>
@@ -17,16 +21,17 @@ export default async function Feed({searchParams}) {
       <h1>main feed</h1>
       
       {show !== "true" && (
-        <div>
-          {posts.rows.map((post) => (
-            <div key={post.id}>
-              <Link href={`/feed/${post.id}`}>
-                <h3>{post.post_content}</h3>
-              </Link>
-            </div>
-          ))}
-        </div>
-      )}
+          <div>
+            {posts.rows.map((post) => (
+              <div key={post.id}>
+                <Link href={`/feed/${post.id}`}>
+                  <h3>{post.post_content}</h3>
+                </Link>
+                <Link href={`/profile/${post.profile_id}`}><p className={styles.username}>by: {post.username}</p></Link>
+              </div>
+            ))}
+          </div>
+        )}
 
       {show === "true" && <CreateProfileModal />}
 
